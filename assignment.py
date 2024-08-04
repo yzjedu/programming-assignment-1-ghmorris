cbc8ad9d95dccace4ba1089d20a881d9a478c9fd
Grace <hayleyagrace@gmail.com>
2024-08-03 20:06:31 -0400

    Assignment 1

--- assignment.py
+++ assignment.py
@@ -1,36 +1,43 @@
-# Name: [full names of all team members]
+# Name: Grace-Ann Morris 
 # Course: CS701/GB735, Dr. Yalew
-# Date: [Submission date]
+# Date: August 6, 2024 
 # Programming Assignment: 1
 # Program Inputs: Dimensions of the room (length, width, height) in feet
 # Program Outputs: Total area to be painted (in square feet), amount of primer (in gallons), amount of paint (in gallons)
 
 def main():
     
-    '''This program Computes the amount of paint needed one more'''
-    # Step 1: Ask the user for the dimensions of the room
-    # The three dimensions are length - height - width 
+    '''This program calculates the total area to be painted (in square feet), amount of primer (in gallons) and amount of paint (in gallons)'''
 
-
-    # Step 2: Compute the total area of the four walls and ceiling
-    # Area of walls = 2 * (length * height + width * height)
-   
-    # Area of ceiling = length * width
-   
-    # Total area = Area of walls + Area of ceiling
-
-
-    # Step 3: Calculate the amount of primer and paint needed
-    # Primer coverage = 200 square feet per gallon
+    # Algorithm Step 1: Input 
+    # Ask user for the dimensions of a room 
+    # Dimensions include: length, width, and ,height
+    length = float (input("What is the length of the room? (in feet) "))
+    width = float (input("What is the width of the room? (in feet)"))
+    height = float(input("What is the height of the room? (in feet)"))
     
-    # Paint coverage = 350 square feet per gallon
+    # Step 2: Calculate the total area of the four walls and ceiling not including the floor 
+    # wall area = 2 * height * (length + width)
+    # ceiling area  = length * width
+    # total area = wall area + ceiling area 
+    wall_area = 2 * height * (length + width)
+    ceiling_area = length * width 
+    total_area = wall_area + ceiling_area
 
+    # Step 3: Compute the amount of primer and paint needed
+    # Define constants: coverage of primer, coverage of paint
+    # coverage of primer = 200 square feet per gallon
+    # coverage of paint = 350 square feet per gallon
+    coverage_of_primer = 200 
+    coverage_of_paint = 350 
+    primer_needed = total_area / coverage_of_primer 
+    paint_needed = total_area / coverage_of_paint
 
-    # Step 4: Output the total area and the amount of primer and paint needed
-    # Uncomment the three lines below for the output
-    # print(f"Total area to be painted: {total_area:.2f} square feet")
-    # print(f"Amount of primer needed: {primer_needed:.2f} gallons")
-    # print(f"Amount of paint needed: {paint_needed:.2f} gallons")
+    # Step 4: Output 
+    # Print the total area and the amount of primer and paint needed
+    print(f"Total area to be painted: {total_area:.2f} square feet")
+    print(f"Amount of primer needed: {primer_needed:.2f} gallons")
+    print(f"Amount of paint needed: {paint_needed:.2f} gallons")
 
 if __name__ == "__main__":
     main()
